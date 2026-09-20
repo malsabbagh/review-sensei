315f495e094e53dd3fc135a32825b45b8cb1c9eb
Mohamad Alsabbagh <mohamad@alsabbagh.io>
2026-09-20 18:14:12 -0400

    fix: remove unused parent import

--- src/review_sensei/hosting/github/application.py
+++ src/review_sensei/hosting/github/application.py
@@ -17,7 +17,6 @@ from ...convergence import (
 from ...conversation import ConversationService
 from ...coverage import coverage_approval_state
 from ...diff import analyze_diff
-from ...disposition import MaintainerCommand
 from ...errors import ReviewInputError
 from ...models import ReviewResult, ReviewTransaction
 from ...outcomes import RecoveryArtifact
